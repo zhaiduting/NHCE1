e8f907c37226cd33a68c561d67328065ac7f7268
zhaiduting <zhaiduting@163.com>
2023-07-20 17:06:44 +0800

    source={{uri: 'file:///android_asset/index.htm'}}

--- App.tsx
+++ App.tsx
@@ -2,7 +2,12 @@ import {WebView} from 'react-native-webview';
 
 function App() {
   return (
-    <WebView source={{uri: 'https://www.youku.com/'}} style={{marginTop: 20}} />
+    <WebView
+      source={{uri: 'file:///android_asset/index.htm'}}
+      allowFileAccess={true}
+      allowFileAccessFromFileURLs={true}
+      originWhitelist={['file://*']}
+    />
   );
 }
 
