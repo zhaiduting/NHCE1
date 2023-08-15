926a0c03b6d4627f10c04029e9ca91932f5fab07
zhaiduting <zhaiduting@icloud.com>
2023-08-15 15:03:02 +0800

    ios

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
 import {WebView} from 'react-native-webview';
+import {Platform} from 'react-native';
 
 function App() {
+  const source =
+    Platform.OS === 'ios'
+      ? './pages/index.htm'
+      : 'file:///android_asset/index.htm';
   return (
     <WebView
-      source={{uri: 'file:///android_asset/index.htm'}}
+      source={{uri: source}}
       allowFileAccess={true}
       allowFileAccessFromFileURLs={true}
       originWhitelist={['file://*']}
